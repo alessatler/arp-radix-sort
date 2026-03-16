b0e0b95784340e7bcf085bf625f262e361adcb6b
ales <satleralesmail@gmail.com>
2026-03-16 18:42:32 +0100

    Impl. radix sort

--- main.cpp
+++ main.cpp
@@ -30,58 +30,6 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 
 // TODO ---------------------------------------------------
 
-void getMinMax(int& min, int& max, vector<int>& nums){
-    for(auto num: nums){
-        if (num < min)
-            min = num;
-        else if (num > max)
-            max = num;
-    }
-}
-
-void subtractMin(const int& min, vector<int>& nums) {
-    for (int& num: nums) {
-        num -= min;
-    }
-}
-
-void addMin(const int& min, vector<int>& nums) {
-    for (int& num: nums) {
-        num += min;
-    }
-}
-
-void countingSort(vector<int> &A) {
-    bool negative = false;
-    int min, max;
-    getMinMax(min, max, A);
-
-    if (min < 0) {
-        negative = true;
-        max -= min;
-        subtractMin(min, A);
-    }
-
-    vector<int> C(max+1);
-
-    for (const int num: A) {
-        C[num]++;
-    }
-
-    for (int i = 1; i < C.size(); i++){
-        C[i] += C[i-1];
-    }
-    vector<int> B(A.size());
-    for (int i = A.size()-1; i >= 0; i--){
-        B[C[A[i]]-1] = A[i];
-        C[A[i]]--;
-    }
-    if (negative)
-        addMin(min, B);
-
-    A = B;
-}
-
 vector<int> pridobiD(const vector<int> &vec, int k) {
     vector<int> D;
     for (int a: vec) {
@@ -96,9 +44,25 @@ int main(int argc, const char* argv[]) {
 
     if (argc < 2) return 0;
     if (!Branje_Stevil(A, argv[1])) return 0;
-    
-    for (int k = 0; k < 8; i++) {
-        // TODO
+
+    for (int k = 0; k < 8; k++) {
+        vector<int> D = pridobiD(A, k);
+
+        int count[2] = {0, 0};
+        for (int bit : D) {
+            count[bit]++;
+        }
+
+        count[1] += count[0];
+
+        vector<int> outputA(A.size());
+
+        for (int i = A.size() - 1; i >= 0; i--) {
+            outputA[count[D[i]] - 1] = A[i];
+            count[D[i]]--;
+        }
+
+        A = outputA;
     }
 
     Izpis_Stevil(&A[0], A.size());
